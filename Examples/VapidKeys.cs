e8662abbd46305227a491c41fd37e6a0d39d30d4
Robert Bill <147130488+RobSlgm@users.noreply.github.com>
2026-04-29 14:43:05 +0200

    Add examples to readme

---
+++ Examples/VapidKeys.cs
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/env dotnet
+#:package ClosureOSS.WebPush@2.4.1
+using WebPush;
+
+var vapidKeys = VapidHelper.GenerateVapidKeys();
+Console.WriteLine($"Public key:  {vapidKeys.PublicKey}");
+Console.WriteLine($"Private key: {vapidKeys.PrivateKey}");
+
